7470dee92264534f0e742417a7308e23804efbdb
Andrey Paramonov <github@ndpar.com>
2020-08-15 23:39:22 -0400

    Reformat mkcerts.sh

--- mkcerts.sh
+++ mkcerts.sh
@@ -23,8 +23,10 @@ CN="Intermediate CA" openssl req -new -config openssl-ca.cnf \
     -key int-key.pem -out int-req.pem
 
 # Intermediate CA: sign CSR with CA extensions
-openssl x509 -req -in int-req.pem -CAkey root-key.pem -CA root-cert.pem -days 3650 \
-    -extfile openssl-ca.cnf -extensions ca_extensions -CAcreateserial -out int-cert.pem
+openssl x509 -req -in int-req.pem -days 3650 \
+    -CAkey root-key.pem -CA root-cert.pem -CAcreateserial \
+    -extfile openssl-ca.cnf -extensions ca_extensions \
+    -out int-cert.pem
 
 # Intermediate CA: delete CSR
 rm int-req.pem
@@ -40,11 +42,14 @@ cat root-cert.pem int-cert.pem > fullchain.pem
 openssl ecparam -genkey -name $CURVE -noout -out server-key.pem
 
 # Server: create CSR from private key
-CN="ECDH Cert" openssl req -new -config openssl-ca.cnf -key server-key.pem -out server-req.pem
+CN="ECDH Cert" openssl req -new -config openssl-ca.cnf \
+    -key server-key.pem -out server-req.pem
 
 # Server: sign CSR with DH extensions
-openssl x509 -req -in server-req.pem -CAkey int-key.pem -CA int-cert.pem -days 3600 \
-    -extfile openssl-ca.cnf -extensions dh_cert -CAcreateserial -out server-cert.pem
+openssl x509 -req -in server-req.pem -days 3600 \
+    -CAkey int-key.pem -CA int-cert.pem -CAcreateserial \
+    -extfile openssl-ca.cnf -extensions dh_cert \
+    -out server-cert.pem
 
 # Server: delete CSR
 rm server-req.pem
